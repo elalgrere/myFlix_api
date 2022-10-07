9f64bdb212740718fd0c8477867174c1b75e0825
Miguel Angel <elalgrere@gmail.com>
2022-10-07 11:28:52 +0200

    Update models.js

--- models.js
+++ models.js
@@ -25,9 +25,28 @@ let movieSchema = mongoose.Schema({
     FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
   });
   
+
+  let genreSchema = mongoose.Schema({
+    Title: {type: String, required: true},
+    Description: {type: String, required: true}
+
+  });
+
+  let directorSchema = mongoose.Schema({
+    Name: {type: String, required: true},
+    Bio: {type: String, required: true},
+    Birthday: Date
+
+  });
+  
   let Movie = mongoose.model('Movie', movieSchema);
   let User = mongoose.model('User', userSchema);
+  let Genre = mongoose.model('Genre', genreSchema);
+  let Director = mongoose.model('Director', directorSchema);
   
   module.exports.Movie = Movie;
   module.exports.User = User;
+  module.exports.Genre = Genre;
+  module.exports.Director = Director;
+
 
