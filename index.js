aa16806549c1c93b832c297026382601d90d19d6
elalgrere <108877252+elalgrere@users.noreply.github.com>
2022-10-12 11:25:32 +0200

    Updated

--- index.js
+++ index.js
@@ -20,11 +20,12 @@ let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
 
-mongoose.connect('mongodb://localhost:27017/myFLixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb://localhost:27017/myFLixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 //CREATE
 app.get('/',(req, res) => {
-	res.send('Welcome to myFlix app!');
+	res.send('Welcome to myFlixx app!');
 });
 app.post('/users',
   // Validation logic here for request
