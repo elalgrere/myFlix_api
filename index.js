a16af4d6f8a697408eb37caa914d9f11b0590098
elalgrere <108877252+elalgrere@users.noreply.github.com>
2022-10-28 12:11:03 +0200

    updated passport authentification

--- index.js
+++ index.js
@@ -113,16 +113,16 @@ app.get("/movies", function (req, res) {
       res.status(500).send("Error: " + error);
     });
 });
-//app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
-//	Movies.find()
-//	  .then((movies) => {
-//		res.status(201).json(movies);
-//	  })
-//	  .catch((error) => {
-//		console.error(error);
-//		res.status(500).send('Error: ' + error);
-//	  });
-  //});
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+	Movies.find()
+	  .then((movies) => {
+		res.status(201).json(movies);
+	  })
+	  .catch((error) => {
+		console.error(error);
+		res.status(500).send('Error: ' + error);
+	  });
+  });
 
 
 // Get movies by title
