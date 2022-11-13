ae65e64c17f9ff46b615f9a9c42e8b7beb81e772
Miguel Angel <elalgrere@gmail.com>
2022-11-13 09:35:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 require('dotenv').config()
 
-let allowedOrigins = ['http://localhost:1234', 'https://myflixmoviesapp.herokuapp.com/'];
+let allowedOrigins = ['http://localhost:8080', 'https://myflixmoviesapp.herokuapp.com/'];
 
 
 app.use(cors({
